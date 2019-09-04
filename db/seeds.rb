08c9a91ad1f6ed5418de1f50b9a8c5b48dea4235
ghita tahiri <ghitatahiri8@gmail.com>
2019-09-04 14:57:10 +0100

    better design and msg for goas page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,6 @@ User.create!(first_name: 'Melissa', last_name: 'Brett', email: 'mexl@gmail.com',
 User.create!(first_name: 'Jack', last_name: 'Minor', email: 'jack@gmail.com', password: '123456', role: 'advisor', response_time: '15', budgeting: true, bio: 'Hello my name is Jack!')
 User.create!(first_name: 'Sophie', last_name: 'Pearl', email: 'sophie@gmail.com', password: '123456', role: 'advisor', response_time: '5', budgeting: true, bio: 'Hello my name is Sophie!')
 User.create!(first_name: 'Alex', last_name: 'Willis', email: 'alex@gmail.com', password: '123456', role: 'advisor', response_time: '10', bill_paying: true, bio: 'Hello my name is Alex!')
-User.create!(first_name: 'Kathy', last_name: 'Rhoden', email: 'kathy@gmail.com', password: '123456', role: 'advisor', response_time: '15', bill_paying: true, bio: 'Hello my name is Kathy!')
+User.create!(first_name: 'Kathy', last_name: 'Rhoden', email: 'kathy13@gmail.com', password: '123456', role: 'advisor', response_time: '15', bill_paying: true, bio: 'Hello my name is Kathy!')
 User.create!(first_name: 'Julien', last_name: 'Pel', email: 'julien@gmail.com', password: '123456', role: 'advisor', response_time: '5', bill_paying: true, bio: 'Hello my name is Julien!')
 puts "Finished creating users"
